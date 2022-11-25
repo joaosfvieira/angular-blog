b0bd5e84debdad73714a878b3a4b7d5433f1d635
Joao de Souza <joaosfvieira@hotmail.com>
2022-11-25 14:19:29 -0800

    Desacoplamento dos dados dinâmicos do html. Movidos para as classes typescript utilizando interpolação.

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover: string = 'https://arquivo.devmedia.com.br/noticias/documentacao/documentacao_angular-cli-como-rodar-testes-e-checar-o-codigo_38248.png';
+  @Input()
+  cardTitle: string = 'É lançada a nova versão do Angular';
+  @Input()
+  cardDescription: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  + 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in'
+  + 'voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit'
+  + ' anim id est laborum.';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent {
-
+  @Input()
+  photoCover: string = 'https://marcdias.com.br/wp-content/uploads/2016/07/reactjs.png';
+  @Input()
+  cardTitle: string = 'Essa é a nossa notícia secundária. Ela fica ao lado da página.';
 }
